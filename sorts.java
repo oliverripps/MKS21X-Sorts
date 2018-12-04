a75d67318ce33c98fe04e94583f4d1b7198d154c
Oliver Ripps <oripps@stuy.edu>
2018-12-04 10:58:36 -0500

    have idea for when it has to be altered, now doing by creating a new ary

---
+++ sorts.java
@@ -0,0 +1,27 @@
+public class Sorts{
+  public static void selectionsort(int[] ary){
+    int[] ary2= new int[];
+    for(int i=0;i<)
+    /*
+    int low;
+    int high;
+    int cur;
+    for(int i=0; i<ary.length;i++){
+      if(ary[i]<=low){
+        cur=ary[i];
+        shiftright(ary,i);
+        ary[0]=cur;
+      }
+    }
+  }
+  public static void shiftright(int[] ary, start){
+    for(int i=ary.length;i>start;i++){
+      ary[i+1]=ary[i];
+    }
+    ary[start]=null;
+  }
+  public static void shiftleft(int[] ary){
+
+  }*/
+
+}
