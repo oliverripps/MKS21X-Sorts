5d2b3fd61a6e8cf4af6b0573f9a19461305a1504
Oliver Ripps <oripps@stuy.edu>
2018-12-04 14:45:51 -0500

    tested

--- sort.java
+++ sort.java
@@ -1,20 +1,15 @@
 public class sort{
-  public static int[] selectionsort(int[] ary){
+  public static void selectionsort(int[] ary){
     int templow;
+    int counter = 0;
     for(int i=0;i<ary.length;i++){
       templow=ary[i];
-      for(int p=0; p<ary.length;p++){
+      for(int p=counter; p<ary.length;p++){
         if(ary[p]<templow){
           templow=ary[p];
         }}
       ary[i]=templow;
-      }
-    return ary;
+      counter++;
     }
-
-  public static void main(String[] args){
-    int[] hi = {1,3,6,3,6,3,8};
-    System.out.println(selectionsort(hi));
   }
-
 }
