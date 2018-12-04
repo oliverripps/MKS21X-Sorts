6992e671054f4e72c5db4094b88835a468d6b65a
Oliver Ripps <oripps@stuy.edu>
2018-12-04 14:40:01 -0500

    testing with a return value

--- sort.java
+++ sort.java
@@ -1,38 +1,20 @@
 public class sort{
-  public static void selectionsort(int[] ary){
-    int low;
+  public static int[] selectionsort(int[] ary){
+    int templow;
     for(int i=0;i<ary.length;i++){
-      low=ary[i];
+      templow=ary[i];
       for(int p=0; p<ary.length;p++){
-        if(ary[p]<low){
-          low=ary[p];
+        if(ary[p]<templow){
+          templow=ary[p];
         }}
-      ary[i]=low;
+      ary[i]=templow;
       }
+    return ary;
     }
 
-
-    /*
-    int low;
-    int high;
-    int cur;
-    for(int i=0; i<ary.length;i++){
-      if(ary[i]<=low){
-        cur=ary[i];
-        shiftright(ary,i);
-        ary[0]=cur;
-      }
-    }
-  }*/
-  public static void add(int[] ary, int origin, int newvalue){
-    for(int i=ary.length;i>origin;i++){
-      ary[i+1]=ary[i];
-    }
-    ary[origin]=newvalue;
+  public static void main(String[] args){
+    int[] hi = {1,3,6,3,6,3,8};
+    System.out.println(selectionsort(hi));
   }
-  /*public static void main(args[] String){
-    int[] ary = {1,3,6,3,6,3,8};
-    System.out.println(selectionsort(ary));
-  }*/
 
 }
