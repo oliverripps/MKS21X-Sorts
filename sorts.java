6c78977222680f44dc91d54f1ab67edbe961346f
Oliver Ripps <oripps@stuy.edu>
2018-12-05 10:26:34 -0500

    adding case and removing bad file

--- sorts.java
+++ sorts.java
@@ -1,38 +1,34 @@
-public class sort{
-  public static void selectionsort(int[] ary){
-    int low;
+public class Sorts{
+  public static void selectionSort(int[] ary){
+    int templow;
+    int counter = 0;
+    int current;
+    int currentindex;
     for(int i=0;i<ary.length;i++){
-      low=ary[i];
-      for(int p=0; p<ary.length;p++){
-        if(ary[p]<low){
-          low=ary[p];
+      templow=ary[i];
+      current=ary[i];
+      currentindex=i;
+      for(int p=counter; p<ary.length;p++){
+        if(ary[p]<templow){
+          templow=ary[p];
+          current=ary[i];
+          currentindex=p;
         }}
-      ary[i]=low;
-      }
+      ary[currentindex]=current;
+      ary[i]=templow;
+      counter++;
     }
-
-
-    /*
-    int low;
-    int high;
-    int cur;
-    for(int i=0; i<ary.length;i++){
-      if(ary[i]<=low){
-        cur=ary[i];
-        shiftright(ary,i);
-        ary[0]=cur;
-      }
-    }
-  }*/
-  public static void add(int[] ary, int origin, int newvalue){
-    for(int i=ary.length;i>origin;i++){
-      ary[i+1]=ary[i];
-    }
-    ary[origin]=newvalue;
   }
-  /*public static void main(args[] String){
-    int[] ary = {1,3,6,3,6,3,8};
-    System.out.println(selectionsort(ary));
+  /*private static String toString(int[] ary){
+    String str = "";
+    for(int i=0;i<ary.length;i++){
+      str+=ary[i];
+      str+=",";
+    }
+    return str;
+  }
+  public static void main(String[] args){
+    int[] ary = {5,2,6,64,6,8,3,2};
+    System.out.println(toString(selectionsort(ary)));
   }*/
-
 }
